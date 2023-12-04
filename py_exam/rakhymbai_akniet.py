d2dffa10b400e453b01560f76c4f48942728e524
vibedawg <lolkekakniet@mail.ru>
2023-12-04 16:35:34 +0600

    04.12.2023 python exam

---
+++ py_exam/rakhymbai_akniet.py
@@ -0,0 +1,47 @@
+#1
+#B.List
+#2
+#C.append()
+#3
+#B. Remove all key-value pairs
+#4
+#False
+#5
+#A. Removes the last element
+#6
+#A.my_dict={}
+#7
+#B.Updates an existing key's value
+#8
+#True
+#9
+#B.. Returns the common elements between two sets
+#10
+#А. [2, 3, 4]
+#11
+#1 you will need a set if you need individual person data
+#2 if you need unique elements
+#12
+# is a sequence of one or more characters
+# numbers = [1,2,3,4,5]
+# num=numbers.index(3)
+# print(num)
+#13
+# my_list =  [1, 2, 3, 4, 5, 6]
+# for x in my_list:
+#     if x % 2==0:
+#       even_num=x+x
+# print(even_num)
+#14
+#fruits = {'apple': 5,'banana': 3}, {'banana': 2, 'orange': 4}
+# i don't know :(
+
+#15
+# fruits =  ["lychee", "apple", "banana", "lemon", "orange", "grape", "lime"]
+# for x in fruits:
+#   if x.startswith("l"):
+#     print(x)
+
+
+
+
